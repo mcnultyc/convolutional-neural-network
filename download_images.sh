13922abdc9faf93fa1123b6c8eda0945b067d72b
mcnultyc <carlos.mcnulty@yahoo.com>
2019-04-17 12:52:20 -0500

    Update download_images.sh

--- download_images.sh
+++ download_images.sh
@@ -24,11 +24,16 @@ fi
 i=0
 while read line
 do	
-	wget $line -t 4 -O "image_$i.jpg"
-	ret="$?"
-	echo $ret
-	if [ $ret -eq 0 ]; then
-		i=$((i + 1))
+	wget $line -q -t 4 -O "image_$i.jpg"
+	wget_ret="$?"
+	if [ $wget_ret -eq 0 ]; then
+		file_output=$(file -b --mime-type "image_$i.jpg")
+		file_ret="$?"
+		if [ $file_ret -eq 0 ] && [ "$file_output" = "image/jpeg" ]; then
+			i=$((i + 1))
+		else
+			rm "image_$i.jpg"
+		fi	
 	else
 		rm "image_$i.jpg"
 	fi
